7fda7b9f149b0171cb2273bee25c8c8d6e4dd303
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-13 05:21:08 +0900

    クエリを扱ったレンダリングに変更

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,11 +1,16 @@
 class HeloController < ApplicationController
 
   def index
+    if params['msg'] != nil then
+      msg = 'hello, ' + params['msg'] + '!'
+    else
+      msg = 'this os sample page.'
+    end 
     msg = '
     <html>
     <body>
       <h2>Sample Page</h2>
-      <p>こんな使い方があるのが(´・ω・｀)ｗ</p>
+      <p>' + msg + '</p>
     </body>
     </html>
     '
