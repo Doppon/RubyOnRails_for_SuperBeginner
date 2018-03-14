a03fb6cd71a6b9df6d3b880053b4a31cddfe1158
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-14 23:50:24 +0900

    Viewを追加したので綺麗にお掃除

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,21 +1,5 @@
 class HeloController < ApplicationController
 
-  def index
-    msg = ''
-    if params[:name].nil?
-      msg = 'this os sample page.'
-    else
-      msg = 'hello, ' + params[:name] + '!'
-    end
-    msg = '
-    <html>
-    <body>
-      <h2>Sample Page</h2>
-      <p>' + msg + '</p>
-    </body>
-    </html>
-    '
-    render html: msg.html_safe
-  end
+  def index; end
 
 end
