84a1c7b96d5ff41e24b5b50385b9f2780f8b81fe
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-16 06:47:04 +0900

    valueとCSRFの記述を削除

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,15 +1,12 @@
 class HeloController < ApplicationController
-  protect_from_forgery
 
   def index
     if request.post?
       @title = 'Result'
       @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params['input1']
     else
       @title = 'デフォのタイトル(´・ω・｀)'
       @msg = 'type text ...'
-      @value = ''
     end
   end
 
