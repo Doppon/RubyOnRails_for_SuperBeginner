724efc9d1dd6b5cfad2a4b4e23a11c5f2633d18a
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-16 07:08:09 +0900

    コントローラー内もラジオボタンの仕様に変更

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -2,15 +2,15 @@ class HeloController < ApplicationController
 
   def index
     if request.post?
-      @title = 'Result'
-      if params[:check1]
-        @msg = 'チェック済み(`･ ω･´)ゞﾋﾞｼｯ!!'
+      @title = '結果は|ω・`）ちら'
+      if params['r1']
+        @msg = '選択したのは┃電柱┃゜艸゜)ジー... ' + params['r1']
       else
-        @msg = 'チェックできてない(っω<。)ﾌｷﾌｷ'
+        @msg = '選択できてない(っω<。)ﾌｷﾌｷ'
       end
     else
       @title = 'Index'
-      @msg = 'Check it ...'
+      @msg = 'ラジオボタンを選択するのじゃ(´・ω・｀)ｗ ...'
     end
   end
 
