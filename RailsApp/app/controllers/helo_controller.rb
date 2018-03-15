d9468cbd6764092af8a51400bbf71f6659466798
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-16 07:10:18 +0900

    少しアレンジ

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -3,7 +3,7 @@ class HeloController < ApplicationController
   def index
     if request.post?
       @title = '結果は|ω・`）ちら'
-      if params['r1']
+      if params[:r1]
         @msg = '選択したのは┃電柱┃゜艸゜)ジー... ' + params['r1']
       else
         @msg = '選択できてない(っω<。)ﾌｷﾌｷ'
