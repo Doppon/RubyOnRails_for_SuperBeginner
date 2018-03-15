521b683535e8e46537f845f0989e859ac1223b11
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-15 13:01:04 +0900

    書式を変更

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,16 +1,16 @@
 class HeloController < ApplicationController
 
   def index
-    if params['msg'].nil?
+    if params[:msg].nil?
       @title = "デフォのタイトル(´・ω・｀)"
     else
-      @title = params['msg']      
+      @title = params[:msg]
     end
     @msg = "リダイレクトの処理を追加したぜいd(´・ω・｀)"
   end
 
   def other
-    redirect_to action: :index, params: {'msg': '待ってこのリダイレクトの書き方初めて'}
+    redirect_to action: :index, params: { msg: '待ってこのリダイレクトの書き方初めて(でもないかｗ)' }
   end
 
 end
