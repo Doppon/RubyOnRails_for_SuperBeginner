9dd4633340d171553b71e79545b356a70d06cfea
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-15 13:07:40 +0900

    パラメータ名を見やすく

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,16 +1,16 @@
 class HeloController < ApplicationController
 
   def index
-    if params[:msg].nil?
+    if params[:query_msg].nil?
       @title = "デフォのタイトル(´・ω・｀)"
     else
-      @title = params[:msg]
+      @title = params[:query_msg]
     end
     @msg = "リダイレクトの処理を追加したぜいd(´・ω・｀)"
   end
 
   def other
-    redirect_to action: :index, params: { msg: '待ってこのリダイレクトの書き方初めて(でもないかｗ)' }
+    redirect_to action: :index, params: { query_msg: '待ってこのリダイレクトの書き方初めて(でもないかｗ)' }
   end
 
 end
