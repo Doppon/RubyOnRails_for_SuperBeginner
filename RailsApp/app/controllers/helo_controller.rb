029235c7463c7112d28bbcc775a2b013cb8f1ff6
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-18 08:50:27 +0900

    当てたレイアウトにパラメータを与える

--- RailsApp/app/controllers/helo_controller.rb
+++ RailsApp/app/controllers/helo_controller.rb
@@ -1,20 +1,12 @@
 class HeloController < ApplicationController
 
+  layout 'helo'
+
   def index
-    if request.post?
-      @title = '結果は|ω・`）ちら'
-      if params['s1']
-        @msg = '選択したのは┃電柱┃゜艸゜)ジー... '
-        for val in params['s1']
-          @msg += val + ' '
-        end
-      else
-        @msg = '選択できてない(っω<。)ﾌｷﾌｷ'
-      end
-    else
-      @title = 'Index'
-      @msg = '色々選択するのじゃ(´・ω・｀)ｗ ...'
-    end
+    @header = 'レイアウトサンプル(´・ω・｀)レイアウトサンプル'
+    @footer = 'DOPO(´・ω・｀)DOPO'
+    @title = '新しいレイアウト(=ﾟωﾟ)ﾉ'
+    @msg = 'this is sample page!(唐突ｗ)'
   end
 
 end
