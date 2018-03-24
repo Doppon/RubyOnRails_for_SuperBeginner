16fc954e4eb1ddfeb6c535e9dd02067b76c11131
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-24 17:41:11 +0900

    peopleのshowルーティングを追加

--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
 
+  get 'people/:id', to: 'people#show'
+
   get 'dengonban/index'
   get 'dengonban', to: 'dengonban#index'
 
