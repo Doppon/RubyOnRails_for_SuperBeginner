02f7bac4cc12f8af49fd3601e006763702ffc576
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-24 18:05:22 +0900

    addアクションの追加

--- RailsApp/app/controllers/people_controller.rb
+++ RailsApp/app/controllers/people_controller.rb
@@ -1,13 +1,17 @@
 class PeopleController < ApplicationController
 
   def index
-    @msg = '(ΦωΦ)フフフ…indexアクションだぜd(´・ω・｀)'    
+    @msg = '(ΦωΦ)フフフ…indexアクションだぜd(´・ω・｀)'
     @data = Person.all
   end
 
   def show
     @msg = '(ΦωΦ)フフフ…showアクションだぜd(´・ω・｀)'
     @data = Person.find(params[:id])
+  end
+
+  def add
+    @msg = '(ΦωΦ)フフフ…addアクションだぜd(´・ω・｀)'
   end
 
 end
