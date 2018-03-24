4907f7aad6b5a6627a3be3b7cb2134f55eb76a38
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-24 19:26:14 +0900

    コントローラーもヘルパーに対応

--- RailsApp/app/controllers/people_controller.rb
+++ RailsApp/app/controllers/people_controller.rb
@@ -12,19 +12,20 @@ class PeopleController < ApplicationController
 
   def add
     @msg = '(ΦωΦ)フフフ…addアクションだぜd(´・ω・｀)'
+    @person = Person.new
   end
-
-  protect_from_forgery
 
   def create
     if request.post? then
-      obj = Person.create(
-        name: params['name'],
-        age: params['age' ],
-        mail: params['mail']
-      )
+      Person.create(person_params)
     end
     redirect_to '/people'
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :age, :mail)
   end
 
 end
