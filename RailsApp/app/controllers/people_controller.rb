3bcd8bb9d72cfde36a041cd43a89bf0f5fbb36cf
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-24 17:37:46 +0900

    showアクションの追加

--- RailsApp/app/controllers/people_controller.rb
+++ RailsApp/app/controllers/people_controller.rb
@@ -1,8 +1,13 @@
 class PeopleController < ApplicationController
 
   def index
-    @msg = 'Person data.'
+    @msg = '(ΦωΦ)フフフ…indexアクションだぜd(´・ω・｀)'    
     @data = Person.all
+  end
+
+  def show
+    @msg = '(ΦωΦ)フフフ…showアクションだぜd(´・ω・｀)'
+    @data = Person.find(params[:id])
   end
 
 end
