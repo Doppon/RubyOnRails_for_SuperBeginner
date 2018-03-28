c759c9f10a8d46d795a7abc4cf54cf7639c2206d
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-29 04:19:09 +0900

    他に必要なcreate/update/deleteアクションを追加

--- RailsApp/app/controllers/messages_controller.rb
+++ RailsApp/app/controllers/messages_controller.rb
@@ -14,6 +14,16 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
