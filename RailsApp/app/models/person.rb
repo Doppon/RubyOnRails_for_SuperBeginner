3f71aa7193e990ddb8ebe7288578e05680d01932
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-29 03:52:18 +0900

    自作バリデーションを適用

--- RailsApp/app/models/person.rb
+++ RailsApp/app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
   validates :name, presence: { message: 'は必要項目だぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
   validates :age, numericality: { message: 'は数字で入力するんだぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
+  validates :mail, email: { message: 'は本当にメアドか？(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
 end
