0eb0d0de364e2cc7b00dd4275380e1cc1e1c039a
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-29 03:22:47 +0900

    エラーメッセージを日本語に

--- RailsApp/app/models/person.rb
+++ RailsApp/app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ApplicationRecord
-  validates :name, presence: true
-  validates :age, numericality: true
+  validates :name, presence: { message: 'は必要項目だぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
+  validates :age, numericality: { message: 'は数字で入力するんだぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
 end
