148d97d622462a4e68d3d920fce7bb30eb06a4b2
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-29 21:50:59 +0900

    post内のadd/edit/deleteコントローラーを定義

--- RailsApp/app/controllers/blogposts_controller.rb
+++ RailsApp/app/controllers/blogposts_controller.rb
@@ -4,12 +4,29 @@ class BlogpostsController < ApplicationController
   end
 
   def add
+    @blogpost = Blogpost.new
+    @genres = Blogpost.all
+    if request.post?
+      @blogpost = Blogpost.create(blogposts_params)
+      redirect_to '/blogposts'
+    end
   end
 
   def edit
+    @blogpost = Blogpost.find(params[:id])
+    @genre = Bloggenre.all
+    if request.patch?
+      @blogpost.update(blogposts_params)
+      redirect_to '/blogposts'
+    end
   end
 
   def delete
+    @blogpost = Blogpost.find(params[:id])
+    if request.post?
+      @blogpost.destroy
+      redirect_to '/blogposts'
+    end      
   end
 
   private
