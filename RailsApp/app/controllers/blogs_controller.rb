c3e0a1668c31b1d03f2f5dcc19158ba40968a373
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-30 02:35:51 +0900

    indexアクションにページの設定を追加

--- RailsApp/app/controllers/blogs_controller.rb
+++ RailsApp/app/controllers/blogs_controller.rb
@@ -1,5 +1,14 @@
 class BlogsController < ApplicationController
   def index
+    page_size = 5
+    @page_num = 0
+    if params[:page].nil?
+      @page_num = params[:page].to_i
+    end
+    @data = Blogpost.all.order('created_at desc')
+      .offset(page_size * @page_num)
+      .limit(page_size)
+    @blogconfig = Blogconfig.find(1)
   end
 
   def genre
