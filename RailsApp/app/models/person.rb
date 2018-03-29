8610d60690f6e8f9f402bc49ba2e188633ca089c
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-29 14:55:50 +0900

    has_oneのアソシエーションを追加

--- RailsApp/app/models/person.rb
+++ RailsApp/app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+  has_one :message
+
   validates :name, presence: { message: 'は必要項目だぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
   validates :age, numericality: { message: 'は数字で入力するんだぞ(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
   validates :mail, email: { message: 'は本当にメアドか？(ﾉ)･∀･(ヾ)ﾓﾁﾓﾁ' }
