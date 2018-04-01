73e58a3d31d9590700128f7ac1a2d2afc3b73349
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-01 15:32:30 +0900

    バリデーションの追加

--- RailsApp/app/models/mycontact.rb
+++ RailsApp/app/models/mycontact.rb
@@ -1,2 +1,5 @@
 class Mycontact < ApplicationRecord
+  validates :name, presence: { message: 'を必要項目です！' }
+  validates :age, numericality: { message: 'を数字で入力してください！' }
+  validates :numericality, inclusion: { in: [true, false], message:'は真偽値です！' }
 end
