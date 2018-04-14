fcb99c6f7c5a0f1497302e5adad4f99ec8016799
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-14 22:56:21 +0900

    calcメソッドの中身を記述

--- RailsApp/app/controllers/questionary_results_controller.rb
+++ RailsApp/app/controllers/questionary_results_controller.rb
@@ -64,6 +64,23 @@ class QuestionaryResultsController < ApplicationController
   end
 
   def calc
+    @questionary = Questionary.find(params[:id])
+    results = QuestionaryResult.where('questionary_id = ?',params[:id])
+    @calc = {}
+    results.each do |result|
+      data = result.result.split(',')
+      data.each do |value|
+        keyval = value.split(':')
+        ky = keyval[0].to_s
+        vl = keyval[1].to_i
+        if ky != 'question_id'
+          if @calc[ky] == nil
+            @calc[ky] = []
+          end
+          @calc[ky][vl] = @calc[ky][vl] == nil ? 1 : @calc[ky][vl].to_i + 1
+        end
+      end
+    end
   end
 
   private
