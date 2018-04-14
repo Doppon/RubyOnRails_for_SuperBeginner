b4d0e0964ce055a5850e29e0cb2f3b58064fe5ef
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-14 22:46:35 +0900

    集計画面のためのルーティングを追加

--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :questionary_items
   resources :questionaries
 
+  get 'questionary_results/calc/:id', to: 'questionary_results#calc'
   get 'questionary_choices/new/:id', to: 'questionary_choices#new'
   get 'questionary_items/new/:id', to: 'questionary_items#new'
 
